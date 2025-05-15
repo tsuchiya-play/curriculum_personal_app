91bc6c097dfda3d8bf48a6a6ac02b8d8f07b9fec
keita.tsuchiya <keita.tsuchiya@PLAY003252.local>
2025-05-15 18:11:08 +0900

    festivalsモデル作成

---
+++ backend/db/schema.rb
@@ -0,0 +1,22 @@
+ActiveRecord::Schema[7.1].define(version: 2025_05_15_072712) do
+  create_table "festivals", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.date "start_date"
+    t.date "end_date"
+    t.text "description"
+    t.string "official_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.string "avatar_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+end
